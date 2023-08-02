d24bb1bea8e8375f861df2e6399489931d61dc28
pandey-9162 <pandey.91621@gmail.com>
2023-08-02 22:55:14 +0530

    final commit

---
+++ app/src/main/java/com/example/weatherapp/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.example.weatherapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        getSupportActionBar()?.hide(); // hide the title bar
+        this.getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
+    }
+}
